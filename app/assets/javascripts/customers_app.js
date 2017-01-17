82fe61b1f41fc65ca23a428f81603b38e0b4b048
sameesh-s <sameesh.fisat@gmail.com>
2017-01-18 00:05:43 +0530

    Angularized first search

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,11 @@
+var app = angular.module('customers', []);
+
+app.controller("CustomerSearchController", [
+    "$scope",
+    function($scope) {
+      $scope.search = function(searchTerm) {
+        $scope.searchedFor = searchTerm;
+      }
+    }
+    ]);
+
