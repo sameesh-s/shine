e627d8a8cafee9f2c5e867a1559919d73284f3bf
sameesh-s <sameesh.fisat@gmail.com>
2017-01-17 12:29:31 +0530

    Indexing transformed values in PostgreSQL'

--- db/structure.sql
+++ db/structure.sql
@@ -171,6 +171,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -206,6 +227,7 @@ SET search_path TO "$user",public;
 
 INSERT INTO schema_migrations (version) VALUES
 ('20170207144228'),
-('20170207144229');
+('20170207144229'),
+('20170207144230');
 
 
